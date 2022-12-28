f761b31c277372f619842d9915b383d496ed98bb
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2022-12-28 12:14:25 +0200

    feat: express server

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
-const http = require("http");
+const app = require("./server");
 
 const PORT = process.env.POST || 3001;
 
-const server = http.createServer((req, res) => {
-  if (req.url !== "/" || req.method !== "GET") {
-    res
-      .writeHead(403, { "Content-Type": "application/json" })
-      .write(JSON.stringify({ message: "Nope!" }));
-    return;
-  }
-
-  res
-    .writeHead(200, { "Content-Type": "application/json" })
-    .write(JSON.stringify({ message: "Hello!" }));
-
-  res.end();
-  return;
-});
-
-server.listen(PORT, () => {
-  console.log(`server is listening at: http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`express server at: http://localhost:${PORT}`);
 });
