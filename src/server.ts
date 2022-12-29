3e001d4bbf5dba58620e013338ca4ca83a6fd8f7
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2022-12-29 15:44:53 +0200

    feat: custom middleware

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,36 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import morgan from "morgan";
+import cors from "cors";
+
 import router from "./router";
+
+/**
+ * @description custom middleware
+ * @param {string} message message that can be passed into middleware
+ * @returns {Function} middleware function
+ */
+const customLogger =
+  (message: string) => (req: Request, res: Response, next: NextFunction) => {
+    console.log("request logged to: ", req.url);
+    console.log("message: ", message);
+    next();
+  };
 
 const app = express();
 
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(customLogger("Message from Serge."));
+
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello!" }).end();
+  res
+    .status(200)
+    .json({
+      message: "Hello!",
+    })
+    .end();
 });
 
 app.use("/api", router);
