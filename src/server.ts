600a43e7bdb7370a3f7a2954b446e2b43f42971b
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2022-12-31 17:51:28 +0200

    feat: added app-level error handler

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,38 @@
-import express, { Request, Response, NextFunction } from "express";
+import express, {
+  Request,
+  Response,
+  NextFunction,
+  ErrorRequestHandler,
+} from "express";
 import morgan from "morgan";
 import cors from "cors";
 
 import { createNewUser, signIn } from "./handlers/users";
 import { protect } from "./modules/auth";
 import router from "./router";
+
+/**
+ * @description an error handler to catch all rest of uncaught errors, THAT happened synchronously ONLY. For all synchronous errors - use trycatch block, where inside catch block you can pass error to next function, then this error will be caught by this function
+ * @example
+ * const endpointDataHandler = async (req, res, next) => {
+ *   try{
+ *      const data = await callSomeApi()
+ *      res.status(200).json({data})
+ *    }catch(error){
+ *      next(error)
+ *    }
+ * }
+ * @param {unknown} error an error thrown
+ * @param {Request} req request service object
+ * @param {Response} res response service object
+ * @param {NextFunction} next service Function to proceed to next
+ * @returns {undefined} handles the incoming error occurence
+ */
+const handleErrors: ErrorRequestHandler = (error, req, res, next) => {
+  const message =
+    error instanceof Error ? error.message : "Server: an error occurred";
+  res.status(500).json({ message });
+};
 
 /**
  * @description custom middleware
@@ -26,7 +54,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(customLogger("Message from Serge."));
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
   res
     .status(200)
     .json({
@@ -38,5 +66,6 @@ app.get("/", (req, res) => {
 app.use("/api", protect, router);
 app.post("/user", createNewUser);
 app.post("/signin", signIn);
+app.use(handleErrors);
 
 export { app };
